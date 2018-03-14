61531685512de396fc364ea9f3d289db0d67e980
Galán <antoniogalan_15@outlook.com>
2018-03-14 23:51:17 +0100

    Script backup3.sh mejorado
    Se ha mejorado el código del script.
    Ahora los ficheros se copian en subdirectorios ordenados por la hora de
    ejecución del script, ubicados en el directorio "backup/fecha(YYMMDD)/".
    De esta forma, al realizar más de una copia de seguridad en el mismo día los
    ficheros anteriores se conservarán, en lugar de sobreescribirse.

--- Scripts/backup3.sh
+++ Scripts/backup3.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-if [ ! -d backup ]	# Comprueba si no existe el directorio "backup"
+# El ejercicio lo he planteado tal y como indica el enunciado, solo
+# que además, he añadido que dentro del directorio compuesto por la
+# fecha, las copias (o backups) se almacenen en subdirectorios según
+# la hora en la que se ejecute este script.
+# 
+# Así los ficheros no se sobreescriben y quedan ordenados y diferenciados
+# en distintos subdirectorios dentro de "backup/fecha(YYMMDD)/".
+
+RUTA=backup	# Ruta hasta el directorio (aumentará con la ejecución).
+
+if [ ! -d $RUTA ]	# Comprueba si no existe el directorio "backup"
 then				# donde se guardarán las copias de seguridad.
-	mkdir backup
+	mkdir $RUTA
 fi
 
-FECHA="`date +%g``date +%m``date +%d`"	# Fecha de la creación de la copia.
 
-if [ ! -d backup/$FECHA ]	# Comprueba si no existe el directorio para
+RUTA=$RUTA/`date +%g``date +%m``date +%d`	# Actualiza la ruta (añade la fecha).
+
+if [ ! -d $RUTA ]	# Comprueba si no existe el directorio para
 then				# esa copia de seguridad (con fecha de hoy).
-	mkdir backup/$FECHA
+	mkdir $RUTA
 fi
 
-# ------------------------------------------------------------------
+RUTA=$RUTA/`date +%T`	# Actualiza la ruta (añade la hora).
+
+if [ ! -d $RUTA ]	# Comprueba si no existe el directorio para
+then				# esa copia de seguridad (con la hora actual).
+	mkdir $RUTA
+fi
+
 for i in $*
 do
-	cp $i backup/$FECHA/$i 	# Copia el fichero original en otro con el mismo nombre.
+	cp $i $RUTA/$i 	# Copia el fichero original ($i) en "RUTA/".
+					# La copia se llamará igual que el original ($i).
 done
-# ------------------------------------------------------------------
+
