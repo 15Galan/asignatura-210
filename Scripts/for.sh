28311511cc231de9bf6aec3e2376e3a1aa875bab
15Galan <antoniogalan_15outlook.com>
2018-03-16 16:20:07 +0100

    Reorganización de las carpetas
    Ahora los ficheros de la práctica introductoria están almacenados en una
    carpeta con el mismo nombre, divididos entre los ejercicios que había
    que entregar y los que no.
    También se ha movido el enunciado de la práctica a esa misma carpeta.

--- Scripts/for.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in `ls`	#for i in * (para todos los archivos del directorio actual)
-do
-	echo $i
-done
--- Scripts/isexec2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e $1 -a -f $1 -a -x $1 ]
-then
-  echo "El fichero "$1" es ejecutable."
-else
-  echo "El fichero "$1" no es algo."
-fi
