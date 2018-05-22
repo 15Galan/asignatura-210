360c5079cad25de8b0bb32ad0089532d50ff022c
Galán <antoniogalan_15@outlook.com>
2018-05-22 12:34:10 +0200

    Archivos modificados
    Se han modificado los archivos de la Práctica Final,
    se ha añadido la Tarea 3.

--- Práctica final/Shell/Shell_project.c
+++ Práctica final/Shell/Shell_project.c
@@ -15,29 +15,73 @@ Para compilar y ejecutar el programa:
 */
 
 #include "job_control.h"	// RECORDAR: Compilar con el modulo "job_control.c".
-#include "string.h"     // Importar para poder usar la funcion "strcmp()".
+#include "string.h"         // Importar para poder usar la funcion "strcmp()".
 
 #define MAX_LINE 256	// 256 caracteres por linea (por comando), deberia ser suficiente.
 
-// -----------------------------------------------------------------------
-//                            MAIN          
-// -----------------------------------------------------------------------
+
+job* tareas;    // Debe declararse global para que pueda usarse por el manejador.
+
+// -----------------------------------------------------------------
+///                          MANEJADOR
+// -----------------------------------------------------------------
+
+void manejador(int signal_id){      /// "signal(SEÑAL, manejador)" espera un "void manejador(int señal)".
+    int pid_wait;                   // PID del proceso en espera.
+    int status;                     // Estado devuelto por "wait()".
+    int info;                       // Informacion procesada por "analyze_status()".
+    enum status status_res;         // Estado procesado por "analyze_status()".
+    job* trabajos = tareas->next;   // Puntero a "tareas" (el primer nodo contiene informacion, no es una tarea).
+
+    while(trabajos != NULL) {
+        pid_wait = waitpid(trabajos->pgid, &status, WUNTRACED | WNOHANG);   // Comprobar para cada proceso.
+
+        status_res = analyze_status(status, &info);     // Estado procesado (Exited, Signaled o Suspended).
+
+        if(pid_wait == trabajos->pgid) {
+            if(status_res == EXITED || status_res == SIGNALED) {    // El proceso ha terminado o envio una señal.
+                printf("\nEl proceso '%s' (pid: %i) ha finalizado.\n", trabajos->command, trabajos->pgid);
+
+                job* auxiliar = trabajos->next; // Guardar temporalmente el puntero al siguiente proceso de la lista.
+
+                delete_job(tareas, trabajos);   // Elimina el proceso "trabajos" de la lista "tareas".
+
+                trabajos = auxiliar;            // Elimina el trabajo haciendo que apunte a donde auxiliar.
+
+            }else{  // SUSPENDED.
+                printf("\nEl proceso '%s' (pid: %i) se ha suspendido.\n", trabajos->command, trabajos->pgid);
+
+                trabajos->state = STOPPED;  // Cambiar estado del proceso a STOPPED.
+                trabajos = trabajos->next;  // Actualizar el trabajo (tarea) al siguiente.
+            }
+
+        }else{
+            trabajos = trabajos->next;  // Actualizar el trabajo (tarea) al siguiente.
+        }
+    }
+}
+
+
+// ----------------------------------------------------------------
+///                              MAIN
+// ----------------------------------------------------------------
 
 int main(void) {
     char inputBuffer[MAX_LINE];     // Buffer para el comando entrante.
     int background;                 // Igual a 1 si el comando acaba con '&'.
     char *args[MAX_LINE / 2];       // Linea de comando de maximo 128 (de 256) argumentos.
 
-    // Variables utiles:
-    int pid_fork, pid_wait;     // pid para procesos creados y en espera.
+    /// Variables utiles:
+    int pid_fork, pid_wait;     // PID para procesos creados (hijos) y en espera.
     int status;                 // Estado devuelto por "wait()".
     enum status status_res;     // Estado procesado por "analyze_status()".
-    int info;                   // Info procesada por "analyze_status()".
+    int info;                   // Informacion procesada por "analyze_status()".
 
-    // Variables utiles (mias):
-    int estado;
+    /// Variables utiles (mias):
+    tareas = new_list("Lista de Tareas");
 
-    ignore_terminal_signals();  // Ignorar señales del Terminal.
+    ignore_terminal_signals();          // Ignorar señales del Terminal.
+    signal(SIGCHLD, manejador);         // Manejar señales SIGCHLD con el "manejador".
 
     while (1) {        // Termina normalmente dentro de "get_command()" tras escribir "^D" ("Ctrl + D").
         printf("COMMAND -> ");  // Prompt (cabecera).
@@ -47,19 +91,13 @@ int main(void) {
 
         if (args[0] == NULL) continue;  // Si se introduce un comando "vacio" se vuelve a pedir.
 
+        /// Comandos Internos ("cd").
         if(strcmp(args[0], "cd") == 0) {     // "strcmp" para comparar Strings.
             if(args[1] != NULL) {
                 chdir(args[1]);     // Cambiar directorio a "args[1]".
 
             }else{
-                chdir(getenv("HOME"));  // "getenv" para convertir "HOME" en la ruta a "/home".
-            }
-
-            estado = analyze_status(status,&info);
-
-            if (info != 1){
-                printf("\nForeground    pid: %i, command: %s, %s, info: %i.\n\n",
-                       getpid(), args[0], status_strings[estado], info);
+                chdir(getenv("HOME"));  // "getenv" para convertir un String en ruta.
             }
 
             continue;
@@ -83,7 +121,7 @@ int main(void) {
 
             printf("ERROR: Command '%s' not found.\n\n", args[0]);
 
-            exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE);     /// DUDA: ¿Por qué "EXIT_FAILURE"?
 
         } else {    // Proceso PADRE.
             new_process_group(pid_fork);    // Crear un grupo de procesos para el proceso padre.
@@ -96,15 +134,25 @@ int main(void) {
 
                 set_terminal(getpid());     // Asigna la Terminal al proceso en ejecucion.
 
-                estado = analyze_status(status, &info);
+                status_res = analyze_status(status, &info); // Estado procesado (Exited, Signaled o Suspended).
+
+                job* proceso = new_job(pid_fork, args[0], FOREGROUND);  // Crear tarea.
+                block_SIGCHLD();            // Bloquear señal SIGCHLD para acceder a una estructura de datos (lista).
+                add_job(tareas, proceso);   // Añadir la tarea "proceso" a la lista "tareas".
+                unblock_SIGCHLD();          // Desbloquear señal SIGCHLD al salir de una estructura de datos (lista).
 
                 /// (4) La Shell muestra un mensaje del estado del comando ejecutado.
                 if (info != 1) {
                     printf("\nForeground    pid: %i, command: %s, %s, info: %i.\n\n",
-                           pid_fork, args[0], status_strings[estado], info);
+                           pid_fork, args[0], status_strings[status_res], info);
                 }
 
             } else {    // Proceso en BACKGROUND.
+                job* proceso = new_job(pid_fork, args[0], BACKGROUND);  // Crear tarea.
+                block_SIGCHLD();            // Bloquear señal SIGCHLD para acceder a una estructura de datos (lista).
+                add_job(tareas, proceso);   // Añadir la tarea "proceso" a la lista "tareas".
+                unblock_SIGCHLD();          // Desbloquear señal SIGCHLD al salir de una estructura de datos (lista).
+
                 /// (4)  La Shell muestra un mensaje del estado del comando ejecutado.
                 if (info != 1) {
                     printf("\nBackground    job running... pid: %i, %s.\n\n", pid_fork, args[0]);
